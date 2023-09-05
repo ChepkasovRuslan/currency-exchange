5ad9692c63256c30850be4679d53c494f09b0cdf
Ruslan Chepkasov <chepkasov.itrum@gmail.com>
2023-09-05 15:06:30 +0300

    use feign client instead of rest client

--- currency-conversion-service/src/main/java/com/example/currencyconversionservice/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/example/currencyconversionservice/CurrencyConversionController.java
@@ -1,19 +1,19 @@
 package com.example.currencyconversionservice;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
+@RequiredArgsConstructor
 @Slf4j
 public class CurrencyConversionController {
+
+  private final CurrencyExchangeServiceProxy currencyExchangeServiceProxy;
 
   @GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
   public CurrencyConversionDto convertCurrency(
@@ -21,16 +21,8 @@ public class CurrencyConversionController {
     @PathVariable String to,
     @PathVariable BigDecimal quantity
   ) {
-    Map<String, String> uriVariables = new HashMap<>() {{
-      put("from", from);
-      put("to", to);
-    }};
-
-    ResponseEntity<CurrencyConversionDto> responseEntity = new RestTemplate().getForEntity(
-      "http://localhost:8000/currency-exchange/from/{from}/to/{to}", CurrencyConversionDto.class,
-      uriVariables);
-
-    CurrencyConversionDto response = responseEntity.getBody();
+    CurrencyConversionDto response = currencyExchangeServiceProxy.retrieveExchangeValue(from, to);
+    log.info(response.toString());
     return new CurrencyConversionDto(response.getId(), from, to, response.getConversionMultiple(), quantity,
       quantity.multiply(response.getConversionMultiple()), response.getPort());
   }
