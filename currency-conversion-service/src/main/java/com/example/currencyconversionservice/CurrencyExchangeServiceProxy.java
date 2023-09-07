b150cb1a91f1eb0d0bd1eef31dcc09cf0fd84fb6
Ruslan Chepkasov <chepkasov.itrum@gmail.com>
2023-09-07 13:56:34 +0300

    dockerize and add metrics

--- currency-conversion-service/src/main/java/com/example/currencyconversionservice/CurrencyExchangeServiceProxy.java
+++ currency-conversion-service/src/main/java/com/example/currencyconversionservice/CurrencyExchangeServiceProxy.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "forex-service", url = "localhost:8000")
+@FeignClient(name = "forex-service", url = "${spring.cloud.openfeign.client.config.postClient.url}")
 public interface CurrencyExchangeServiceProxy {
 
   @GetMapping("/currency-exchange/from/{from}/to/{to}")
